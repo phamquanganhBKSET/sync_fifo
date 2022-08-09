a9b50d23481265b0d01e665701bc29ad3503cb35
nguyenvietthi <nguyenvietthi.nd@gmail.com>
2022-08-09 17:22:10 +0700

    update syn scripts

---
+++ syn/dc_scripts/common_setup.tcl
@@ -0,0 +1,48 @@
+################################################################################
+# User-defined Project Path
+################################################################################
+set TOP_LEVEL_MODULE  sync_fifo
+set PROJECT_DIR       ../../sync_fifo
+set SCRIPT_DIR        ../../sync_fifo/syn/dc_scripts
+
+################################################################################
+# RTL Source code
+################################################################################
+set RTL_SOURCE         $PROJECT_DIR/hdl
+set INCLUDE_SOURCE     $PROJECT_DIR/inc
+
+##########################################################################################
+# User-defined variables for logical library setup in dc_setup.tcl
+##########################################################################################
+set SOURCE_PATH "$RTL_SOURCE $INCLUDE_SOURCE";
+
+set ADDITIONAL_SEARCH_PATH        "/ref/libs/mw_lib/sc/LM $RTL_SOURCE $INCLUDE_SOURCE" ;# Directories containing logical libraries,
+                                                                                          # logical design and script files.
+
+set TARGET_LIBRARY_FILES          sc_max.db                   ;#  Logical technology library file
+
+set SYMBOL_LIBRARY_FILES          sc.sdb                      ;#  Symbol library file
+
+##########################################################################################
+# User-defined variables for physical library setup in dc_setup.tcl
+##########################################################################################
+
+set MW_DESIGN_LIB                  SYNC_FIFO_LIB                      ;# User-defined Milkyway design library name
+
+set MW_REFERENCE_LIB_DIRS         /ref/libs/mw_lib/sc                 ;# Milkyway reference libraries
+
+set TECH_FILE                     /ref/libs/tech/cb13_6m.tf           ;#  Milkyway technology file
+
+set TLUPLUS_MAX_FILE              /ref/libs/tlup/cb13_6m_max.tluplus  ;#  Max TLUPlus file
+
+set MAP_FILE                      /ref/libs/tlup/cb13_6m.map          ;#  Mapping file for TLUplus
+
+################################################################################
+# Clock Information
+################################################################################
+
+set CLOCK_PERIOD         5.0
+set MAX_SKEW             [expr {$CLOCK_PERIOD * 0.05}]
+set MAX_JITTER_TIME      [expr {$CLOCK_PERIOD * 0.02}]
+set MARGIN_TIME          [expr {$CLOCK_PERIOD * 0.02}]
+set MAX_TRANSITION_TIME  [expr {$CLOCK_PERIOD * 0.1 }]
---
+++ syn/dc_scripts/run_dc.tcl
@@ -0,0 +1,47 @@
+# This file has been edited to keep it simple
+source -echo -verbose ./scripts/common_setup.tcl
+source -echo -verbose ./scripts/dc_setup.tcl
+
+printvar target_library
+printvar link_library
+
+#################################################################################
+# Read in the RTL Design
+# Read in the RTL source files or read in the elaborated design (.ddc).
+#################################################################################
+
+analyze $SOURCE_PATH -autoread
+analyze -vcs "-sverilog -y $RTL_SOURCE +libext+.v+.sv" ${TOP_MODULE_FILE}  
+elaborate ${TOP_LEVEL_MODULE} -lib work
+# Compile Top module
+current_design ${TOP_LEVEL_MODULE}
+check_design> ${REPORTS_DIR}/${TOP_LEVEL_MODULE}_check_design.rpt
+link
+
+#################################################################################
+# Read contraint file
+#################################################################################
+source -echo -verbose ./dc_scripts/constraints.tcl
+
+set_cost_priority -delay
+
+compile_ultra 
+
+#################################################################################
+# Write out Design
+#################################################################################
+write -format ddc     -hierarchy -output ${RESULTS_DIR}/${TOP_LEVEL_MODULE}_netlist.ddc
+write -format verilog -hierarchy -output ${RESULTS_DIR}/${TOP_LEVEL_MODULE}_netlist.v
+
+################################################################################
+# Generate Final Reports
+#################################################################################
+check_timing > ${REPORTS_DIR}/${TOP_LEVEL_MODULE}_check_timing.rpt
+report_timing -nworst 100 -sort_by slack -normalized_slack   > ${REPORTS_DIR}/${TOP_LEVEL_MODULE}_final_timing.rpt
+report_constraint -all_violators                             > ${REPORTS_DIR}/${TOP_LEVEL_MODULE}_final_constraints.rpt
+
+if {[shell_is_in_topographical_mode]} {
+  report_area -physical -nosplit > ${REPORTS_DIR}/${TOP_LEVEL_MODULE}_final_area.rpt
+} else {
+  report_area -nosplit > ${REPORTS_DIR}/${TOP_LEVEL_MODULE}_final_area.rpt
+}
